54efdb6e7827b7607acd7888e2e36ab0b2035c0b
root <root@pop-os.localdomain>
2021-02-19 00:25:03 -0300

    simple sidebar

--- resources/kickstart/layout/backend.blade.php
+++ resources/kickstart/layout/backend.blade.php
@@ -3,6 +3,10 @@
 @section('header-logo')
     Exemple
 @endsection
+@section('header-usermenu')
+    <a class="dropdown-item d-flex align-items-center" href="{{ env('URL_AUTH') }}">Voltar<i class="fa fa-arrow-left fa-fw ml-auto text-gray-400 f-s-16"></i></a>
+    <div class="dropdown-divider"></div>
+@endsection
 @section('sidebar')
     @include('layout.partials.sidebar')
 @endsection
--- resources/views/layout/partials/sidebar.blade.php
+++ resources/views/layout/partials/sidebar.blade.php
@@ -1,19 +1,21 @@
 
 <div id="sidebar" class="app-sidebar">
-    <div class="app-sidebar-content" data-scrollbar="true" data-height="100%">
-        <div class="menu">
-            <div class="menu-header ">Menu</div>
-            <div class="menu-item {{ str_contains(request()->url(), '/index') ? 'active' : '' }}">
-                <a href="{{ route('admin_index') }}" class="menu-link ">
-                    <span class="menu-icon"><i class="fa fa-home"></i></span>
-                    <span class="menu-text">Dashboard</span>
-                </a>
+    @if (trim($__env->yieldContent('sidebar')))
+
+        @yield('sidebar')
+
+    @else
+
+        <div class="app-sidebar-content" data-scrollbar="true" data-height="100%">
+            <div class="menu">
+
+                @yield('sidebar-menu')
+
             </div>
-
-            @yield('sidebar')
-
         </div>
-    </div>
 
-    <button class="app-sidebar-mobile-backdrop" data-dismiss="sidebar-mobile"></button>
+        <button class="app-sidebar-mobile-backdrop" data-dismiss="sidebar-mobile"></button>
+
+    @endif
+
 </div>
