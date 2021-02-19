f056e852dc96add57547c6ba66efdbe754128580
root <root@pop-os.localdomain>
2021-02-19 00:32:19 -0300

    ending

--- resources/views/layout/partials/sidebar.blade.php
+++ resources/views/layout/partials/sidebar.blade.php
@@ -1,21 +1,22 @@
 
-<div id="sidebar" class="app-sidebar">
-    @if (trim($__env->yieldContent('sidebar')))
-
-        @yield('sidebar')
-
-    @else
-
-        <div class="app-sidebar-content" data-scrollbar="true" data-height="100%">
-            <div class="menu">
-
-                @yield('sidebar-menu')
-
+@if( strlen(trim($__env->yieldContent('sidebar'))) or strlen(trim($__env->yieldContent('sidebar-menu'))))
+    <div id="sidebar" class="app-sidebar">
+        @if (trim($__env->yieldContent('sidebar')))
+            <div class="app-sidebar-content" data-scrollbar="true" data-height="100%">
+                <div class="menu">
+                    @yield('sidebar')
+                </div>
             </div>
-        </div>
-
-        <button class="app-sidebar-mobile-backdrop" data-dismiss="sidebar-mobile"></button>
-
-    @endif
-
-</div>
+            <button class="app-sidebar-mobile-backdrop" data-dismiss="sidebar-mobile"></button>
+        @else
+            <div class="app-sidebar-content" data-scrollbar="true" data-height="100%">
+                <div class="menu">
+                    @yield('sidebar-menu')
+                </div>
+            </div>
+            <button class="app-sidebar-mobile-backdrop" data-dismiss="sidebar-mobile"></button>
+        @endif
+    </div>
+@else
+    <style> .app-content { margin-left: auto!important; }</style>
+@endif
